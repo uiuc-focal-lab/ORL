517a881043b8d2e1e8398b2489104a5a33b25ac2
yaboidav3 <davidzhuyuer@outlook.com>
2024-05-22 14:56:26 -0500

    script changes / remove

--- edac2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for seed in 1 2
-do
-    python CORL/algorithms/offline/edac.py --project "Experiments_CORL" --env_name "walker2d-medium-v2" --out_name "edac-walker-medium-original-no_overlap"               --num_t 24000  --len_t 20 --latent_reward 0 --bin_label 0 --train_seed $seed --eval_seed $seed
-    python CORL/algorithms/offline/edac.py --project "Experiments_CORL" --env_name "walker2d-medium-v2" --out_name "edac-walker-medium-binary_labels-no_overlap"          --num_t 24000  --len_t 20 --latent_reward 0 --bin_label 1 --train_seed $seed --eval_seed $seed
-    python CORL/algorithms/offline/edac.py --project "Experiments_CORL" --env_name "walker2d-medium-v2" --out_name "edac-walker-medium-latent_reward-no_overlap"          --num_t 24000  --len_t 20 --latent_reward 1 --bin_label 0 --train_seed $seed --eval_seed $seed
-done
-
--- iql2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for seed in 1 2
-do
-    python CORL/algorithms/offline/iql.py --project "Experiments_CORL" --env "walker2d-medium-replay-v2" --out_name "iql-walker-medium-replay-original-no_overlap"               --num_t 7000  --len_t 20 --latent_reward 0 --bin_label 0 --seed $seed
-    python CORL/algorithms/offline/iql.py --project "Experiments_CORL" --env "walker2d-medium-replay-v2" --out_name "iql-walker-medium-replay-binary_labels-no_overlap"          --num_t 7000  --len_t 20 --latent_reward 0 --bin_label 1 --seed $seed
-    python CORL/algorithms/offline/iql.py --project "Experiments_CORL" --env "walker2d-medium-replay-v2" --out_name "iql-walker-medium-replay-latent_reward-no_overlap"          --num_t 7000  --len_t 20 --latent_reward 1 --bin_label 0 --seed $seed
-done
-
